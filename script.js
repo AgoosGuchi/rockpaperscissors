961ba2847185146eeba7b8e6cde18ceff98f9665
Agos Terracino <69160466+AgoosGuchi@users.noreply.github.com>
2023-02-11 14:15:11 -0300

    fixes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const options = ["Rock","Paper","Scissors"];
+const options = ["rock","paper","scissors"];
 const message = alert("Let's Play Rock, paper, scissors! Choose your option:")
 
 function computerPlay() {
@@ -7,25 +7,41 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection){
-    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+    if (playerSelection === computerSelection) {
         return "It's a tie!";
-    }
-    else if (playerSelection.toLowerCase() === "Rock" && computerSelection.toLowerCase() === "Scissors"
-    || playerSelection.toLowerCase() === "Paper" && computerSelection.toLowerCase() === "Rock"
-    || playerSelection.toLowerCase() === "Scissors" && computerSelection.toLowerCase() === "Paper"){
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
         return "You win! " + playerSelection + " beats " + computerSelection;
-    } else if(computerSelection.toLowerCase() === "Rock" && playerSelection.toLowerCase() === "Scissors"  || computerSelection.toLowerCase() === "Paper" && playerSelection.toLowerCase() === "Rock" || computerSelection.toLowerCase() === "Scissors" && playerSelection.toLowerCase() === "Paper"){
+    } else {
         return "You lose! " + computerSelection + " beats " + playerSelection;
     }
 }
 
-var playerCount = 0;
-var computerCount = 0;
+let playerCount = 0;
+let computerCount = 0;
+
+function userChoice(){
+    let selection = prompt("Rock, paper, or scissors?");
+    if (selection === null) {
+        return "Match cancelled"
+    }
+    while (
+        selection.toLowerCase() !== "rock" &&
+        selection.toLowerCase() !== "paper" &&
+        selection.toLowerCase() !== "scissors"
+    ) {
+        selection = prompt("Invalid selection. Please enter rock, paper, or scissors.");
+    }
+    return selection;
+}
 
 function game(){
     for (let i = 0; i < 5; i++){
-        const playerSelection = prompt("Rock, Paper or Scissors?");
-        const computerSelection = computerPlay();
+        let playerSelection = userChoice()
+        let computerSelection = computerPlay()
         let result = playRound(playerSelection, computerSelection);
         if (result.startsWith("You win")) {
             playerCount++;
@@ -35,16 +51,24 @@ function game(){
         console.log("Round " + ( i+1 ) + " : " + result);
     }
 }
+function matchResult(){
+    console.log("Final Score: Player " + playerCount + " Computer " + computerCount);
+    if (playerCount < computerCount) {
+        console.log("You lost the match. Computer wins with " + computerCount + " points")
+    } else if (playerCount > computerCount) {
+        console.log("You win the match with " + playerCount + " points")
+    } else {
+        console.log("The match its tied!")
+    };
+    console.log("If you want to play again type replay() in the console")
+}
+function replay(){
+    playerCount = 0;
+    computerCount = 0;
+    game();
+    matchResult();
+}
 
 message;
-game()
-
-console.log("Final Score: Player " + playerCount + " Computer " + computerCount);
-
-if (playerCount < computerCount) {
-    console.log("You lost the match. Computer wins with " + computerCount + " points")
-} else if (playerCount > computerCount) {
-    console.log("You win the match with " + playerCount + " points")
-} else {
-    console.log("The match its tied!")
-};
+game();
+matchResult();
